e80e895ac8ed1f36b78742d83207932b8afbf53f
David Forbes <davidforbesi@hotmail.com>
2023-07-19 16:59:45 +0100

    created library, add book, remove book and display library functions

---
+++ script.js
@@ -0,0 +1,75 @@
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const readYes = document.querySelector('#yes');
+const readNo = document.querySelector('#no');
+const button = document.querySelector('button');
+const containerDiv = document.querySelector('.container');
+let read;
+
+let myLibrary = [
+  {
+    title: 'The Hobbit',
+    author: 'Tolkien',
+    pages: 345,
+    read: 'yes'
+  }
+];
+
+displayLibrary();
+
+function displayLibrary() {
+  containerDiv.innerHTML = '';
+  myLibrary.forEach((book, index) => {
+    const div = document.createElement('div');
+    const title = document.createElement('h2');
+    title.textContent = `Title: ${book['title']}`;
+    const author = document.createElement('p');
+    author.textContent = `Author: ${book['author']}`;
+    const pages = document.createElement('p');
+    pages.textContent = `Pages: ${book['pages']}`;
+    const read = document.createElement('p');
+    read.textContent = `Read: ${book['read']}`;
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Delete';
+    div.appendChild(title);
+    div.appendChild(author);
+    div.appendChild(pages);
+    div.appendChild(read);
+    div.appendChild(removeButton);
+    containerDiv.appendChild(div);
+    div.setAttribute('Index', index);
+    removeButton.setAttribute('Index', index);
+  })
+  allRemoveButtons = containerDiv.querySelectorAll('button');
+  allRemoveButtons.forEach(item => item.addEventListener('click', () => {
+    const index = item.getAttribute('index');
+    const div = document.querySelector(`div[index="${index}"]`);
+    containerDiv.removeChild(div);
+    myLibrary.splice(index, index + 1);
+  }))
+}
+
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  displayLibrary();
+}
+
+button.addEventListener('click', () => {
+  if (readYes.checked === true) {
+    read = 'yes';
+  } else {
+    read = 'no';
+  }
+  const book = new Book(title.value, author.value, pages.value, read);
+  addBookToLibrary(book);
+});
+
+
