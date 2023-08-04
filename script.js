ca0fda2ea75c3f595f4c092b8c58df233ab8654b
David Forbes <davidforbesi@hotmail.com>
2023-08-04 11:24:51 +0100

    changed book to class instead of regular constructor

--- script.js
+++ script.js
@@ -12,11 +12,13 @@ const booksRead = document.querySelector('div.books-read');
 
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+  }
 }
 
 formButton.addEventListener('click', () => {
